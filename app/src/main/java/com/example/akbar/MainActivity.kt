5328baf991e3bc6590cc18178b8bfea801ddf7ee
jkososo12 <kulijawaveteran@gmail.com>
2025-01-11 20:24:07 +0700

    Pengetesan Pertama

--- app/src/main/java/com/example/akbar/MainActivity.kt
+++ app/src/main/java/com/example/akbar/MainActivity.kt
@@ -2,16 +2,19 @@ package com.example.akbar
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.akbar.ui.theme.AkbarTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,10 +25,26 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Design(){
-
+    Column {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Image(
+                painter = painterResource(id = R.drawable.images)
+                , contentDescription = "ini gambar",
+                modifier= Modifier.size(30.dp)
+            )
+            Text("Chico Lachowski",
+                color = colorResource(R.color.white)
+            )
+        }
+        Image(
+            painter = painterResource(id = R.drawable.img)
+            , contentDescription = "ini gambar",
+            modifier= Modifier.size(150.dp)
+        )
+    }
 }
 @Composable
 @Preview
 fun Display(){
-
+    Design()
 }
